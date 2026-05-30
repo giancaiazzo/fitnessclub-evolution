4c6bd38b846efa11d7c9384954c1eb58da0c68de
GianCaia <gian34567@gmail.com>
2026-05-30 15:53:51 -0300

    chore: FRONT-ENTD INICIALIZADO React/Typescript/TailwindCSS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <main className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        FitnessClubEvolution
+      </h1>
+    </main>
+  )
+}
+
+export default App
