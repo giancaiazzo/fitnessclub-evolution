8db20b8be166764ea184d80f7183d3d4d671471a
GianCaia <gian34567@gmail.com>
2026-06-24 11:06:20 -0300

    Update ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -2,8 +2,7 @@ import ContactForm from "../MOD1-CLIENTES/components/ContactForm";
 import Layout from "../layouts/Layout";
 import { contactInfo, siteData, socialLinks } from "../MOD1-CLIENTES/data/siteData";
 
-const mapImage =
-  "https://images.unsplash.com/photo-1517487881594-2787fef5ebf7?w=1200&h=600&fit=crop&q=80";
+
 
 export default function ContactPage() {
   return (
