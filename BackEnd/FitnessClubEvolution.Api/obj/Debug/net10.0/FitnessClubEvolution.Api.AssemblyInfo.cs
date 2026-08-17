b92bff8b32e140f95284599d9514ad7476d1ebf4
GianCaia <gian34567@gmail.com>
2026-08-17 14:01:09 -0300

    v2
    pronto m1 y m2

--- BackEnd/FitnessClubEvolution.Api/obj/Debug/net10.0/FitnessClubEvolution.Api.AssemblyInfo.cs
+++ BackEnd/FitnessClubEvolution.Api/obj/Debug/net10.0/FitnessClubEvolution.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessClubEvolution.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a11d32dfaa0d0fd218ed8f3334692ea3d1f6ab9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a51b1afcf1acb24081abcfa46b4286eb11386d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessClubEvolution.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessClubEvolution.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
